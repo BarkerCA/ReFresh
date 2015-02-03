4cf6382e259f82adbb1fb27a331e4216908b20b0
Chris Barker <chris.a.barker@icloud.com>
2015-02-03 13:13:08 -0500

    Static assets unconfigured

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module ReFresh
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.serve_static_files = true
+    #config.serve_static_files = true
     
   end
 end
